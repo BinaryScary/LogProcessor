1360db107b6c4317db970438712981cd9bfa7eca
Sam Wong <sam_wong156@hotmail.com>
2019-03-07 19:09:27 -0500

    Restructured to not not focus on tests

--- logProcessor.py
+++ logProcessor.py
@@ -7,48 +7,49 @@ class LogProcessor:
         user = ''
         passScheme = ''
         time = ''
-        init = None
-        succ = None
+        # init = None
+        # succ = None
         with open(filename, 'rU') as f:
             for line_terminated in f:
                 line = line_terminated.rstrip('\n')
 
-                if self.checkTest(line): 
-                    continue
+                # if self.checkTest(line): 
+                #     continue
                 user = self.getUser(line)
                 passScheme = self.getScheme(line)
                 time = self.getTime(line)
                 init = self.isInit(line)
-                if init == False:
-                    succ = self.getSucc(line)
-                print(user, passScheme, time, init, succ)
+                # if init == False:
+                #     succ = self.getSucc(line)
+                print(user, passScheme, time )
+                print(line)
 
-    def getSucc(self, line):
-        temp = line.split()[3]
-        temp = temp[0:temp.find(';')]
-        if temp == 'textrandom':
-            if line.split()[5] == 'good':
-                return True
-            else:
-                return False
-        elif temp == 'passtiles':
-            if line.split()[4] == 'goodPractice':
-                return True
-            else:
-                return False
+    # def getSucc(self, line):
+    #     temp = line.split()[3]
+    #     temp = temp[0:temp.find(';')]
+    #     if temp == 'textrandom':
+    #         if line.split()[5] == 'good':
+    #             return True
+    #         else:
+    #             return False
+    #     elif temp == 'passtiles':
+    #         if line.split()[4] == 'goodPractice':
+    #             return True
+    #         else:
+    #             return False
 
     
-    def isInit(self, line):
-        init = [ 'Created', 'Password' ]
+    # def isInit(self, line):
+    #     init = [ 'Create', 'Password' ]
 
-        if line.split()[4] in init:
-            return True
-        else:
-            return False
+    #     if line.split()[4] in init:
+    #         return True
+    #     else:
+    #         return False
     
     def checkTest(self, line):
-        entries = [ 'Created', 'pwtest', 'Password', 'goodPractice', 'badPractice' ]
-
+        # TODO: redo for Enter succ / fail
+        entries = [ 'Create', 'pwtest', 'Password', 'goodPractice', 'badPractice' ]
         if line.split()[4] in entries:
             return True
         else:
@@ -68,5 +69,5 @@ class LogProcessor:
 
 if __name__ == '__main__':
     log = LogProcessor()
-    log.process("test")
+    log.process("text21.txt")
     print("done")
